ebbfdca1c087a846686d9a0dae2d3de3b7c3de3e
neverouo <1019832705@qq.com>
2022-11-22 15:37:06 +0800

    图片自由Demo

--- lib/picture_free_demo.dart
+++ lib/picture_free_demo.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:ui' as ui;
+import 'package:image/image.dart' as imgLib;
 import 'package:flutter/material.dart';
 
 class PictureFreePage extends StatefulWidget {
@@ -15,6 +16,9 @@ class _PictureFreePageState extends State<PictureFreePage> {
   Image? image1 = Image.network('https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farchive%2F088f45fa6c18352278ece24321e87a124ef28787.jpg&refer=http%3A%2F%2Fi0.hdslb.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1671688899&t=3f906b8eedd5d1da784f9472663f9375');
   Completer<ui.Image> completer = Completer<ui.Image>();
   ui.Image? info;
+
+  List<imgLib.Image>? parts;
+
   @override
   void initState() {
     super.initState();
@@ -28,7 +32,25 @@ class _PictureFreePageState extends State<PictureFreePage> {
     info = await completer.future;
     print("1");
     print(image1!.height);
+
+    imgLib.Image? image2 = imgLib.decodeImage(image1!.image.);
+
+    int x = 0;
+    int y = 0;
+
+    // for (int i = 0; i < 3; i++) {
+    //   for (int j = 0; j < 3; j++) {
+    //     parts!.add(imgLib.copyCrop(image1!, x, y, 288, 288));
+    //     x += 288;
+    //   }
+    //   x = 0;
+    //   y += 288;
+    // }
+
+
   }
+
+
 
   @override
   void dispose() {
@@ -51,40 +73,26 @@ class _PictureFreePageState extends State<PictureFreePage> {
           child: ListView(
             children: [
               image1!,
-              ClipRect(
-                clipper: MyClipper(
-                  index: 1,
-                  gcd: 288,
-                ), //使用自定义的clipper
-                child: image1!,
-              ),
-              ClipRect(
-                clipper: MyClipper(
-                  index: 2,
-                  gcd: 288,
-                ), //使用自定义的clipper
-                child: image1!,
-              ),
-              GridView.builder(
-                physics: NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 8,
-                  childAspectRatio: 1, //显示区域宽高相等
-                  mainAxisSpacing: 5,
-                  crossAxisSpacing: 5,
-                ),
-                itemCount: 40,
-                itemBuilder: (context,index){
-                  return ClipRect(
-                    clipper: MyClipper(
-                      index: index + 1,
-                      gcd: 288,
-                    ), //使用自定义的clipper
-                    child: image1!,
-                  );
-                },
-              ),
+              // GridView.builder(
+              //   physics: NeverScrollableScrollPhysics(),
+              //   shrinkWrap: true,
+              //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              //     crossAxisCount: 8,
+              //     childAspectRatio: 1, //显示区域宽高相等
+              //     mainAxisSpacing: 5,
+              //     crossAxisSpacing: 5,
+              //   ),
+              //   itemCount: 40,
+              //   itemBuilder: (context,index){
+              //     return ClipRect(
+              //       clipper: MyClipper(
+              //         index: index + 1,
+              //         gcd: 288,
+              //       ), //使用自定义的clipper
+              //       child: image1!,
+              //     );
+              //   },
+              // ),
               GestureDetector(
                 onTap: () async{
                   int width = info!.width;
@@ -120,3 +128,31 @@ class MyClipper extends CustomClipper<Rect> {
   @override
   bool shouldReclip(CustomClipper<Rect> oldClipper) => false;
 }
+
+// List<Image> splitImage(List<int> input) {
+//   // convert image to image from image package
+//   imglib.Image image = imglib.decodeImage(input);
+//
+//   int x = 0, y = 0;
+//   int width = (image.width / 3).round();
+//   int height = (image.height / 3).round();
+//
+//   // split image to parts
+//   List<imglib.Image> parts = List<imglib.Image>();
+//   for (int i = 0; i < 3; i++) {
+//     for (int j = 0; j < 3; j++) {
+//       parts.add(imglib.copyCrop(image, x, y, width, height));
+//       x += width;
+//     }
+//     x = 0;
+//     y += height;
+//   }
+//
+//   // convert image from image package to Image Widget to display
+//   List<Image> output = List<Image>();
+//   for (var img in parts) {
+//     output.add(Image.memory(imglib.encodeJpg(img)));
+//   }
+//
+//   return output;
+// }
